713b3916ec264a7849114d21213d6ed837ae3218
ankita-0123 <pragyanparamita1449@gmail.com>
2023-06-06 19:07:34 +0530

    commit id

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ShowList from './components/ShowList';
+import ShowSummary from './components/ShowSummary';
+import TicketBookingForm from './components/TicketBookingForm';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ShowList />} />
+        <Route path="/summary/:id" element={<ShowSummary />} />
+        <Route path="/booking/:id" element={<TicketBookingForm />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
+
+
+
